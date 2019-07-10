2d2e1a22de372b186a30025d0a02c1c5894b8912
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-10 22:11:10 +0800

    add a function to colculate quantity

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,15 @@
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+function colculateItemQuantity(){
+  let barcodesArray = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+  var objItem={}
+  for(let item of barcodesArray){
+    var itemArray=item.replace(/\s+/g,"").split("x")
+    let key=itemArray[0]
+    let quantity=Number(itemArray[1])
+    objItem[key]=quantity add 
+  }
+  return objItem
+}
+colculateItemQuantity()
